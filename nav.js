7fe2402e788577d45e3d5b9c803b14cc50dfd09f
AneeshBist <aneeshbist@gmail.com>
2023-06-14 23:13:59 +0530

    express html & json

--- nav.js
+++ nav.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the home page");
+  //res.send("<h1>Welcome to the home page</h1>");
+  res.write("<h1>Welcome written to home page</h1>");
+  res.send();
 });
 app.get("/about", (req, res) => {
   res.send("This is the ABOUT page");
@@ -11,8 +13,39 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.send("Contact page");
 });
+// app.get("/temp", (req, res) => {
+//   res.send([
+//     {
+//       id: 1,
+//       name: "billy",
+//     },
+//     {
+//       id: 2,
+//       name: "tim",
+//     },
+
+//     {
+//       id: 3,
+//       name: "joice",
+//     },
+//   ]);
+// });
 app.get("/temp", (req, res) => {
-  res.send("A Temporary page is being shown");
+  res.json([
+    {
+      id: 1,
+      name: "billy",
+    },
+    {
+      id: 2,
+      name: "tim",
+    },
+
+    {
+      id: 3,
+      name: "joice",
+    },
+  ]);
 });
 app.listen(port, () => {
   console.log(`Listening to port: ${port}`);
