aaa7270354433996931498459a02242ff58fe7b2
AneeshBist <aneeshbist@gmail.com>
2023-06-16 23:29:06 +0530

    template engines - using hbs on simple html page

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ const app = express();
 //console.log(__dirname);
 const staticPath = path.join(__dirname, "../public");
 
+//set view engine
+app.set("view engine", "hbs");
+
 //built in middleware
-app.use(express.static(staticPath));
+//app.use(express.static(staticPath));
+
+// template
+app.get("/", (req, res) => {
+  res.render("index", {
+    firstname: "Joe",
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello from the other side");
