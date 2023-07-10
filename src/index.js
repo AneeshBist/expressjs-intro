43170ca4d57b4cfb8330373a3c4ca18a9b0d00a7
AneeshBist <aneeshbist@gmail.com>
2023-07-10 23:52:43 +0530

    url paramters usage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const hbs = require("hbs");
+const requests = require("requests");
 
 //console.log(__ dirname) ;
 const staticPath = path.join(__dirname, "../public");
@@ -24,11 +25,31 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  //console.log(req.query);
-  res.render("about", {
-    fname: req.query.fname,
-    lname: req.query.lname,
-  });
+  if (req.url == "/") {
+    requests(
+      `https://api.openweathermap.org/data/2.5/weather?q=${req.query.name}&units=metric&appid=219e680aeb5a2cab7cf3cccec33e903e`
+    )
+      .on("data", (chunk) => {
+        const jsonData = JSON.parse(chunk);
+        const arrData = [jsonData];
+        console.log(
+          `City name is ${arrData[0].name} & temp is ${arrData[0].main.temp}`
+        );
+
+        res.write(arrData[0].name);
+        //console.log(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+
+    //console.log(req.query);
+    // res.render("about", {
+    //   fname: req.query.fname,
+    //   lname: req.query.lname,
+    // });
+  }
 });
 
 // app.get("/", (req, res) => {
